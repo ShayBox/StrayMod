8a7805cd6f0961d47a164f3d5684c43fdb15b010
Shayne Hartford <shayneehartford@gmail.com>
2019-06-13 21:22:32 -0400

    1.0.2 - Fixed crash

--- src/main/java/com/shaybox/straymav/Main.java
+++ src/main/java/com/shaybox/straymav/Main.java
@@ -10,11 +10,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import java.time.LocalDateTime;
 import java.util.Timer;
 
-@Mod(modid = Main.MOD_ID, name = Main.MOD_NAME, version = Main.VERSION, dependencies = "require-after:chancecubes")
+@Mod(modid = Main.MOD_ID, name = Main.MOD_NAME, version = Main.VERSION, dependencies = "required-after:chancecubes")
 public class Main {
 	static final String MOD_ID = "straymav";
 	static final String MOD_NAME = "StrayMav";
-	static final String VERSION = "1.0.1";
+	static final String VERSION = "1.0.2";
 
 	@Mod.Instance(MOD_ID)
 	static Main INSTANCE;
